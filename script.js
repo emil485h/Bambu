0c1e52e6b2474cec96b3acccb69d51ffbb6a455b
Katrine <drys@live.dk>
2022-05-31 23:38:13 +0200

    Fixed burger & added fonts

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-linksm')[0]
+// const toggleButton = document.getElementsByClassName('toggle-button')[0]
+// const navbarLinks = document.getElementsByClassName('navbar-linksm')[0]
 
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
-})
+// toggleButton.addEventListener('click', () => {
+//     navbarLinks.classList.toggle('active')
+// })
+
+function openMenu() {
+    var x = document.getElementById("navbar-li");
+    if (x.style.display === "flex"){
+        x.style.display = "none";
+    } else {
+        x.style.display = "flex";
+    }
+}
